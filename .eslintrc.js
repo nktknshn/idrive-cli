4300181744345e320b1089c29ed5ad7d65c927a9
Nikita Kanashin <nikita@kanash.in>
2021-10-10 19:28:20 +0300

    adding linting

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  rules: {
+    'id-length': ['error', { min: 1, max: 30 }],
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+}
