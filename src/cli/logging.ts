02ab60e7da83bb368d5d822aa175f122179e81db
Nikita Kanashin <nikita@kanash.in>
2024-08-21 17:44:28 +0300

    add time logging to api requests

--- src/cli/logging.ts
+++ src/cli/logging.ts
@@ -1,6 +1,8 @@
 import { logTimeRTE, logTimeSRTE, logTimeTE } from '../util/log-time'
 import { timeLoggerIO } from '../util/logging'
 
+export const debugTimeIO = logTimeIO(timeLoggerIO.debug)
+
 export const debugTimeTE = logTimeTE(timeLoggerIO.debug)
 export const debugTimeRTE = logTimeRTE(timeLoggerIO.debug)
 export const debugTimeSRTE = logTimeSRTE(timeLoggerIO.debug)
--- src/util/log-time.ts
+++ src/util/log-time.ts
@@ -1,12 +1,16 @@
 import * as t from 'fp-ts-contrib/lib/time'
 import { pipe } from 'fp-ts/lib/function'
+import * as IO from 'fp-ts/lib/IO'
 import * as RTE from 'fp-ts/lib/ReaderTaskEither'
 import * as SRTE from 'fp-ts/lib/StateReaderTaskEither'
 import * as TE from 'fp-ts/lib/TaskEither'
 
 export const timeTE = t.time(TE.MonadTask)
+export const timeIO = t.time(TE.MonadTask)
 
 type Logger = (msg: string) => () => void
+
+// export const logTimeIO = (logger: Logger) => (name: string) => t.time(IO.MonadIO)
 
 export const logTimeTE = (logger: Logger) =>
   (name: string) =>
